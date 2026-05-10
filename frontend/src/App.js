1af15839b43b4deaad3213537dc83466e3f7813c
Lanrewest <lanrewese@yahoo.com>
2026-05-10 23:19:31 +0100

    admin page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import Store from "./pages/Store"; // .jsx is implied
 import GeneralStore from "./pages/GeneralStore"; // .jsx is implied
 import LandingPage from "./pages/LandingPage.jsx"; // .jsx is explicit for clarity
 import NotFound from "./pages/NotFound";
+import AdminDashboard from "./pages/AdminDashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />{" "}
         <Route path="/store" element={<GeneralStore />} />{" "}
         <Route path="/store/:slug" element={<Store />} />{" "}
         <Route path="/" element={<LandingPage />} />{" "}
